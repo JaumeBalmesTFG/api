4303b5f7c32b838bbc30ce6ca4f18237a8c07aa9
davidortegafarrerons <atreidesd32@gmail.com>
2022-03-14 16:59:00 +0100

    [F-14] MODIFIED server.js and auth.js to set auth endpoints in server so Levon can work on register endpoint.

--- server.js
+++ server.js
@@ -25,11 +25,34 @@ const ufRouter = require("./routes/uf/uf");
 app.use(express.json());
 app.use(morgan('dev'));
 
+/**
+ * Routes
+ */
 app.use("/module", moduleRouter);
 app.use("/rule", ruleRouter);
 app.use("/task", taskRouter);
 app.use("/truancy", truancyRouter);
 app.use("/uf", ufRouter);
+
+/**
+ * Auth routes (Pending to move to a single file)
+ */
+app.post("/auth", req, res => {
+    //Check if req email is in db
+    //Return json with "action": "login|register" format
+});
+
+app.post("/register", req, res => {
+    //Check that all the data is valid (no empty data in comparation with the model and email is well formatted).
+    //Check that user does not exist
+    //Store user
+    //Return message and code
+});
+
+app.post("/login", req, res => {
+    //Check that email and password are correct
+    //Return token & code
+});
 
 // Testing endpoint
 app.get('/', function (req, res) {
