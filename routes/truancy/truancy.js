0bbb402149627d42bdd3ddd84c67373623a981e7
davidortegafarrerons <atreidesd32@gmail.com>
2022-04-06 20:49:53 +0200

    [F-23] [WIP] Created joi schema validator, updated truancy and truancyController, error in truancy.js

--- routes/truancy/truancy.js
+++ routes/truancy/truancy.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+// Controllers
+const {
+    delete,
+    create,
+    update,
+    get,
+} = require('../../controllers/truancy/truancyController');
+
+// Middlewares
+const { isAuthenticatedPrivate } = require('../../middlewares/auth/authentication');
+const { validateTruancySchema } = require('../../middlewares/truancy/truancyValidator');
+
+// Auth
+router.use(isAuthenticatedPrivate);
+
+/**
+ * CRUD
+ */
+router.post("/create", validateTruancySchema, create);
+router.put("/:truancy_id", validateTruancySchema, update)
+router.get("/:truancy_id", get);
+router.delete("/:truancy_id/delete",
+
+
+
+
+
 router.post("/create", function(req, res) {
     //Check data is valid (Compare with the model)
     //Return message and code
