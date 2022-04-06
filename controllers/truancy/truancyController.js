d36f35050da5c2c895fcb170b40d514a778a1c6b
davidortegafarrerons <atreidesd32@gmail.com>
2022-04-06 20:34:11 +0200

    [F-23] [WIP] Created truancy controller and truancy model

---
+++ controllers/truancy/truancyController.js
@@ -0,0 +1,180 @@
+// Truancy Model
+const Truancy = require('../../models/truancy/Truancy');
+
+// Status Messages
+const {
+    HttpStatusCode,
+    HttpStatusMessage,
+    ResponseMessage
+} = require('../../config/status-codes');
+
+// Checker
+const {
+    checkPathObjectId
+} = require('../../services/checker');
+
+// Create Truancy
+exports.create = async function (req, res, next) {
+
+    const { ufId, date, reason, hours } = req.body;
+
+    const match = await Truancy.findOne({
+        ufId: ufId,
+        name: name
+    });
+
+    if (match) {
+        return res.status(HttpStatusCode.CONFLICT).send({
+            message: ResponseMessage.ALREADY_EXISTS,
+            path: req.originalUrl,
+            method: req.method,
+            body: req.body
+        });
+    }
+
+    const newTruancy = new Truancy({
+        ufId: ufId,
+        date: date,
+        reason: reason,
+        hours: hours
+    });
+
+    await newTruancy.save(function (err, doc) {
+        if (err) {
+            return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
+                error: ResponseMessage.DATABASE_ERROR,
+                path: req.originalUrl,
+                method: req.method,
+                body: req.body
+            });
+        }
+
+        return res.status(HttpStatusCode.CREATED).send({
+            message: 'TRUANCY_' + ResponseMessage.CREATED,
+            path: req.originalUrl,
+            method: req.method,
+            body: doc,
+        });
+    });
+}
+
+// Get Truancy
+exports.get = async function (req, res, next) {
+
+    if(!checkPathObjectId(req.params.truancy_id)){
+        return res.status(HttpStatusCode.BAD_REQUEST).send({
+            message: HttpStatusMessage.BAD_REQUEST,
+            path: req.originalUrl,
+            method: req.method,
+            body: req.body
+        });
+    }
+
+    await Truancy.findOne({ _id: req.params.truancy_id }, function (err, doc) {
+        if (err) {
+            return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
+                message: ResponseMessage.DATABASE_ERROR,
+                path: req.originalUrl,
+                method: req.method,
+                body: doc,
+            });
+        }
+
+        return res.status(HttpStatusCode.OK).send({
+            message: HttpStatusMessage.OK,
+            path: req.originalUrl,
+            method: req.method,
+            body: doc,
+        });
+    });
+}
+
+// Update Truancy
+exports.update = async function (req, res, next) {
+
+    if(!checkPathObjectId(req.params.truancy_id)){
+        return res.status(HttpStatusCode.BAD_REQUEST).send({
+            message: HttpStatusMessage.BAD_REQUEST,
+            path: req.originalUrl,
+            method: req.method,
+            body: req.body
+        });
+    }
+
+    const { ufId, date, reason, hours } = req.body;
+
+    const doc = await Truancy.findOne({ _id: req.params.truancy_id});
+
+    if(!doc){
+        return res.status(HttpStatusCode.NOT_FOUND).send({
+            message: HttpStatusMessage.NOT_FOUND,
+            path: req.originalUrl,
+            method: req.method,
+            body: req.body
+        });
+    }
+
+    doc.date = date
+    doc.reason = reason
+    doc.hours = hours
+
+    await doc.save(function (err, obj){
+        if (err) {
+            return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
+                error: ResponseMessage.DATABASE_ERROR,
+                path: req.originalUrl,
+                method: req.method,
+                body: req.body
+            });
+        }
+
+        return res.status(HttpStatusCode.OK).send({
+            message: HttpStatusMessage.OK,
+            path: req.originalUrl,
+            method: req.method,
+            body: obj,
+        });
+    });
+}
+
+// Delete Truancy
+exports.archive = async function (req, res, next) {
+
+    if(!checkPathObjectId(req.params.truancy_id)){
+        return res.status(HttpStatusCode.BAD_REQUEST).send({
+            message: HttpStatusMessage.BAD_REQUEST,
+            path: req.originalUrl,
+            method: req.method,
+            body: req.body
+        });
+    }
+
+    const match = await Module.findOne({  _id: req.params.truancy_id });
+
+    if(!match){
+        return res.status(HttpStatusCode.NOT_FOUND).send({
+            message: HttpStatusMessage.NOT_FOUND,
+            path: req.originalUrl,
+            method: req.method,
+            body: req.body
+        });
+    }
+
+    await match.delete(function(err, doc){
+        if (err) {
+            return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
+                error: ResponseMessage.DATABASE_ERROR,
+                path: req.originalUrl,
+                method: req.method,
+                body: req.body
+            });
+        }
+
+        return res.status(HttpStatusCode.OK).send({
+            message: HttpStatusMessage.OK,
+            path: req.originalUrl,
+            method: req.method,
+            body: doc,
+        });
+    })
+}
