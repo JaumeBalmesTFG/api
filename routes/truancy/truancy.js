3062c2d2dbff214f8d1d894eb59b23313e4f64bb
davidortegafarrerons <atreidesd32@gmail.com>
2022-04-07 16:47:04 +0200

    [F-23] Remove unnecessary endpoints

--- routes/truancy/truancy.js
+++ routes/truancy/truancy.js
@@ -23,28 +23,4 @@ router.post("/create", validateTruancySchema, create);
 router.put("/:truancy_id/edit", validateTruancySchema, update);
 router.get("/:truancy_id", get);
 router.delete("/:truancy_id/delete", remove);
-
-
-
-
-
-router.post("/create", function(req, res) {
-    //Check data is valid (Compare with the model)
-    //Return message and code
-});
-
-router.get("/:truancy_id", function(req, res) {
-    //Return all the data of the truancy_id after checking it exists
-    //Return message and code
-});
-
-router.put("/:truancy_id/edit", function(req, res) {
-    //Check that truancy_id exists and data is valid.
-    //Return message and code
-});
-
-router.delete("/:truancy_id/delete", function(req, res) {
-    //check that truancy_id exsits and delete it
-});
-
 module.exports = router;
