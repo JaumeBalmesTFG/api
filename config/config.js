73a874f2403a4163c98bcff58fdc7de45abdf02c
xavigrausebastian <xavier.grau@kreatings.com>
2022-04-25 16:59:53 +0200

    Change URI_TEST connection string and database

--- config/config.js
+++ config/config.js
@@ -5,10 +5,10 @@ module.exports = {
   PORT: process.env.PORT || 8080,
 
   // Databse connection uri
-  URI: process.argv[2] || "mongodb://127.0.0.1:27017/testAPI",
+  URI: process.argv[2] || "mongodb://127.0.0.1:27017/KlendarAPI",
 
   // Test Mocha Database Uri
-  URI_TEST:  process.argv[2] || "mongodb://127.0.0.1:27017/test",
+  URI_TEST: "mongodb://db:27017/TestKlendarAPI",
 
   // Number of rounds for the password encryption
   ROUNDS: 8,
@@ -17,6 +17,6 @@ module.exports = {
   HOST: "http://localhost:8080",
 
   // Token Generation Secrey Key
-  SECRET: 'secret',
+  SECRET: '#(7B.+dA,p8@pPD}D%x|M]Y1gT"[1=EwB`/RNoaI^[{-7p`i>4/X,Qf]x;i$K',
 };
 
