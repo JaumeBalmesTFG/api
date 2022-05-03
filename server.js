d467ca0805e8d69ddbf3d642063c2caf932f6602
xavigrausebastian <xavier.grau@kreatings.com>
2022-05-03 17:38:59 +0200

    Install cors again...

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const cors = require('cors');
 // Petition Debugger
 const morgan = require('morgan');
 
@@ -29,13 +29,14 @@ const testRouter = require('./routes/test');
 // Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
-app.use((req, res, next) => {
+app.use(cors());
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+});*/
 
 // Mochawesome
 app.use('/test', express.static(__dirname + '/public/mocha'));
